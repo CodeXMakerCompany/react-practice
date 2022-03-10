2225371c02642bc8559bf9f15b49c1070fca8ca0
CodeXMakerCompany <codexmaker12@gmail.com>
2022-03-10 10:58:45 -0600

    initial: birth

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+import { CardContent } from './components/card/cardContent.js';
+
+const CardTitle = ({ title }) => <span>{title}</span>;
+
+const Card = ({ object }) => {
+  return (
+    <h3>
+      <CardTitle title={object.title} />
+      <CardContent initialValue={5}/>
+    </h3>
+  );
+};
+
+const App = () => {
+  const item = {
+    title: "🚀",
+    subtitle: "b",
+  };
   return (
     <div className="App">
+      <Card object={item} />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <p>lorem ipsum dolor sit amet, consectetur adip</p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +37,6 @@ function App() {
       </header>
     </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/card/cardContent.js
@@ -0,0 +1,20 @@
+import React, { useEffect, useState } from "react";
+
+export const CardContent = ({initialValue}) => {
+  const [count, setCount] = useState(initialValue);
+
+  const addOrSomething = () => {
+    if ((count - 1) <= 0) {
+      return alert("'THAT'S NOT RIGHT")
+    }
+    setCount(count - 1)
+  };
+  
+  return (
+    <div>
+      <p>Adding more and more cards</p>
+      <p>{count}</p> <br />
+      <button onClick={() => addOrSomething()}>Hit me!</button>
+    </div>
+  );
+};
